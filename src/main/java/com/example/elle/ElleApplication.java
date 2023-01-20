84c0ffd3360d2289b24a8b1210d9de450191ec4a
RYChiy <meinrachid2372001@gmail.com>
2023-01-20 14:58:28 +0100

    nnew project

---
+++ src/main/java/com/example/elle/ElleApplication.java
@@ -0,0 +1,13 @@
+package com.example.elle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/elle/ElleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
